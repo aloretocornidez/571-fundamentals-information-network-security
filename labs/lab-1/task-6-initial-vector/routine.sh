ee323a9b53605e140104cb11a41b750474ece5d4
Alan Loreto Cornidez <aloretocornidez@arizona.edu>
2024-02-14 22:38:26 -0700

    remove files

--- labs/lab-1/task-6-initial-vector/routine.sh
+++ labs/lab-1/task-6-initial-vector/routine.sh
@@ -10,7 +10,7 @@ key2=aabbccddeeff00112233445566778889
 iv1=0102030405060708
 iv2=0506070801020304
 
-input="./test.txt"
+input="words"
 
 
 
@@ -18,23 +18,33 @@ input="./test.txt"
 openssl enc -aes-128-ofb -e -in ${input} -out ./encrypted-iv1-k1.txt  -K ${key1} -iv ${iv1}
 
 # do part a
-openssl enc -aes-128-ofb -e -in ${input} -out ./encrypted-iv1-k2.txt  -K ${key2} -iv ${iv1}
+openssl enc -aes-128-ofb -e -in ${input} -out ./encrypted-iv1-k2.txt  -K ${key1} -iv ${iv1}
 
 # do part b
 openssl enc -aes-128-ofb -e -in ${input} -out ./encrypted-iv2-k2.txt  -K ${key2} -iv ${iv2}
 
-# bless ./test.txt ./encrypted-iv1-k1.txt ./encrypted-iv1-k2.txt ./encrypted-iv2-k2.txt
+# generate a hexdump of the files.
+hexdump ./encrypted-iv1-k1.txt > ./encrypted-iv1-k1.hex
+hexdump ./encrypted-iv1-k2.txt > ./encrypted-iv1-k2.hex
+hexdump ./encrypted-iv2-k2.txt > ./encrypted-iv2-k2.hex
 
+clear
+echo "Diff"
+diff ./encrypted-iv1-k1.hex ./encrypted-iv1-k2.hex 
+echo "Diff Complete"
+# diff ./encrypted-iv1-k1.hex ./encrypted-iv1-k2.hex ./encrypted-iv2-k2.hex
+
+rm *.txt *.hex
 
 # guessing the plain text now, 
 # task 6.2
 
 
-p1="This is a known message!" 
-c1="a469b1c502c1cab966965e50425438e1bb1b5f9037a4c159"
-c2="bf73bcd3509299d566c35b5d450337e1bb175f903fafc159"
+# p1="This is a known message!" 
+# c1="a469b1c502c1cab966965e50425438e1bb1b5f9037a4c159"
+# c2="bf73bcd3509299d566c35b5d450337e1bb175f903fafc159"
 
-./sample_code.py
+# ./sample_code.py
 
 # task 6.3 
 
